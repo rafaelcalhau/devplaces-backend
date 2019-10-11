14e432a7352b043e27bbdac78acba6df35d7a292
Rafael Calhau <rafael@calhau.me>
2019-10-10 21:49:02 -0300

    Updated env file and added https option to start the server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,25 @@
+import fs from 'fs'
+import https from 'https'
+import path from 'path'
 import app from './app'
 
+const { useHTTPS, sslPrivateKey, sslCert } = process.env
 const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+if (useHTTPS === 'true') {
+  // eslint-disable-next-line
+  const privateKey = fs.readFileSync(path.resolve(__dirname, '..', '..', 'ssl', sslPrivateKey), 'utf-8')
+  // eslint-disable-next-line
+  const certificate = fs.readFileSync(path.resolve(__dirname, '..', '..', 'ssl', sslCert), 'utf-8')
+
+  const credentials = { key: privateKey, cert: certificate }
+  const httpsServer = https.createServer(credentials, app)
+
+  httpsServer.listen(port, () => {
+    console.log(`Server is running on port ${port} (https)`)
+  })
+} else {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port} (http)`)
+  })
+}
